c2bc44a3f25ce32c894e95e5db28fd79472ed7c9
awais512 <raza.awais25@gmail.com>
2023-11-17 01:02:55 +0000

    Theme and dark mode setup

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,7 +1,11 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
-    <p className="text-3xl font-bold text-indigo-500">
-      This is protected route
-    </p>
+    <div>
+      <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
+    </div>
   );
 }
