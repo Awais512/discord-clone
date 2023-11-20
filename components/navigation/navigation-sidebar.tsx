7fcd12f28482a8cfe361e0ee7d523621c07c22b4
awais512 <raza.awais25@gmail.com>
2023-11-20 00:33:29 +0000

    Showing server in sidebar

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -2,6 +2,9 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
 import NavigationActions from "./navigation-actions";
+import { Separator } from "../ui/separator";
+import { ScrollArea } from "../ui/scroll-area";
+import { NavigationItem } from "./navigation-item";
 
 const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -22,6 +25,18 @@ const NavigationSidebar = async () => {
   return (
     <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
       <NavigationActions />
+      <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+      <ScrollArea className="flex-1 w-full">
+        {servers.map((server) => (
+          <div className="mb-4" key={server.id}>
+            <NavigationItem
+              id={server.id}
+              imageUrl={server.imageUrl}
+              name={server.name}
+            />
+          </div>
+        ))}
+      </ScrollArea>
     </div>
   );
 };
