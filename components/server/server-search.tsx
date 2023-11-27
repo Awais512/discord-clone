97077bdda5645c18eab179421b69d1dd64198921
awais512 <raza.awais25@gmail.com>
2023-11-27 00:07:10 +0000

    onClick with type of channels or members

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { SearchIcon } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import {
   CommandDialog,
   CommandEmpty,
@@ -10,6 +10,7 @@ import {
   CommandItem,
   CommandList,
 } from "../ui/command";
+import { useParams, useRouter } from "next/navigation";
 
 interface ServerSearchProps {
   data: {
@@ -27,6 +28,37 @@ interface ServerSearchProps {
 
 export const ServerSearch = ({ data }: ServerSearchProps) => {
   const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const params = useParams();
+
+  useEffect(() => {
+    const down = (e: KeyboardEvent) => {
+      if ((e.key === "k" && e.metaKey) || e.ctrlKey) {
+        e.preventDefault();
+        setOpen((open) => !open);
+      }
+    };
+
+    document.addEventListener("keydown", down);
+
+    return () => document.removeEventListener("keydown", down);
+  }, []);
+
+  const onClick = ({
+    id,
+    type,
+  }: {
+    id: string;
+    type: "channel" | "member";
+  }) => {
+    setOpen(false);
+    if (type === "member") {
+      return router.push(`/servers/${params?.serverId}/conversations/${id}`);
+    }
+    if (type === "channel") {
+      return router.push(`/servers/${params?.serverId}/channels/${id}`);
+    }
+  };
 
   return (
     <>
@@ -53,7 +85,7 @@ export const ServerSearch = ({ data }: ServerSearchProps) => {
             <CommandGroup key={label} heading={label}>
               {data?.map(({ id, icon, name }) => {
                 return (
-                  <CommandItem key={id}>
+                  <CommandItem onSelect={() => onClick({ id, type })} key={id}>
                     {icon}
                     <span>{name}</span>
                   </CommandItem>
